86a3c1a12625cf33b5243cf8f59846997b95a558
Fred Mitchell <flajann@gmail.com>
2015-11-29 10:33:39 +0100

    in progress of putting in the cool

--- lib/rubyneat_rabbitmq.rb
+++ lib/rubyneat_rabbitmq.rb
@@ -2,6 +2,7 @@
 require 'rubyneat'
 require 'bunny'
 require 'thor'
+require 'rubyneat'
 require_relative 'rubyneat_rabbitmq/cli'
 require_relative 'rubyneat_rabbitmq/dsl'
 require_relative 'rubyneat_rabbitmq/monkey'
